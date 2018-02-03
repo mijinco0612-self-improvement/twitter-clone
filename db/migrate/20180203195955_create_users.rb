0c84208e66abb46f9b73b14e9c9d3f26e81f87b0
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-02-04 06:39:59 +0900

    add user model & test

--- db/migrate/20180203195955_create_users.rb
+++ db/migrate/20180203195955_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :emailrails generate migration add_index_to_users_email
+      t.string :email
 
       t.timestamps
     end
