007b3dd9fbc71d632034a54092defa28a1e1fa37
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-02-13 14:06:34 +0900

    add production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # for session store.
+  config.session_store_servers = ENV["REDISCLOUD_URL"]
+
 end
