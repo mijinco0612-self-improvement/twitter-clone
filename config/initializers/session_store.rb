55823a4019379fcd461a1c8e06a4ae1cad7e6fd7
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-02-13 18:44:10 +0900

    fix code style

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :redis_store, servers: Rails.application.config.session_store_servers, expire_after: 10.minute
+Rails.application.config.session_store :redis_store,
+                                       servers: Rails.application.config.session_store_servers,
+                                       expire_after: 10.minute
