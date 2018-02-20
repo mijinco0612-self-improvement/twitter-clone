3a483c94d76b084cca1903741d05d7f7942ff144
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-02-20 18:45:08 +0900

    add edit user

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+
+  # for session store.
+  config.session_store_servers = 'redis://localhost:6379/0/session'
 end
