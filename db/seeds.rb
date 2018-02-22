7f57f78decdca69b839002e0901ead91c01f1b79
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-02-23 05:00:57 +0900

    view micro posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create!(name: "Example User",
-             email: "example@test.org",
+User.create!(name: "test takamichi",
+             email: "test@gmail.com",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -16,4 +16,10 @@ User.create!(name: "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each {|user| user.microposts.create!(content: content)}
 end
